a95fe66c47536f03ee58029ab0f95c9337aafe5b
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2026-06-25 15:48:46 +0200

    Hand crafted Simple DAE FMU
    * Taken from https://github.com/modelica/fmi-ls-dae/issues/48
    * FMU was created by masoudnajafi
    
    Co-authored-by: masoudnajafi <33484393+masoud-najafi@users.noreply.github.com>

---
+++ reference-FMUs/SimpleDAE/DAE/sources/config.h
@@ -0,0 +1,40 @@
+#ifndef config_h
+#define config_h
+
+// define class name and unique id
+#define MODEL_IDENTIFIER DAE
+#define INSTANTIATION_TOKEN "{BD403596-3166-4232-ABC2-DAE}"
+
+#define MODEL_EXCHANGE
+
+#define MAX_CONTINUOUS_STATES 2
+
+#define SET_FLOAT64
+
+#define GET_PARTIAL_DERIVATIVE
+
+#define FIXED_SOLVER_STEP 1e-2
+#define DEFAULT_STOP_TIME 20
+
+typedef enum {
+    vr_time, vr_x1, vr_der_x1, vr_x2, vr_der_x2, vr_z1,
+    vr_z2, vr_u1, vr_u2, vr_y1, vr_y2, vr_res1, vr_res2
+} ValueReference;
+
+typedef struct {
+    double x1;
+    double der_x1;
+    double x2;
+    double der_x2;
+    double z1;
+    double z2;
+    double u1;
+    double u2;
+    double y1;
+    double y2;
+    double res1;
+    double res2;
+
+} ModelData;
+
+#endif /* config_h */
