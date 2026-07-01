c125f784c94e1a4040cff336c951315568a8290e
masoudnajafi <33484393+masoud-najafi@users.noreply.github.com>
2026-07-01 18:52:34 +0200

    Update config.h to include ode/dae switch support

--- reference-FMUs/SimpleDAE/config.h
+++ reference-FMUs/SimpleDAE/config.h
@@ -1,5 +1,6 @@
 #ifndef config_h
 #define config_h
+#include <stdbool.h> // for bool
 
 // define class name and unique id
 #define MODEL_IDENTIFIER SimpleDAE
@@ -10,7 +11,8 @@
 #define MAX_CONTINUOUS_STATES 2
 
 #define SET_FLOAT64
-
+#define GET_BOOLEAN
+#define SET_BOOLEAN
 #define GET_PARTIAL_DERIVATIVE
 
 #define FIXED_SOLVER_STEP 1e-2
@@ -18,7 +20,8 @@
 
 typedef enum {
     vr_time, vr_x1, vr_der_x1, vr_x2, vr_der_x2, vr_z1,
-    vr_z2, vr_u1, vr_u2, vr_y1, vr_y2, vr_res1, vr_res2
+    vr_z2, vr_u1, vr_u2, vr_y1, vr_y2, vr_res1, vr_res2,
+    vr_ode_dae
 } ValueReference;
 
 typedef struct {
@@ -34,7 +37,7 @@ typedef struct {
     double y2;
     double res1;
     double res2;
-
+    bool ode_dae;
 } ModelData;
 
 #endif /* config_h */
